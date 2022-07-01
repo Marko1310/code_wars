92d148a128399d08f52ddaed1f3c38dbf3232ad2
Marko <marko.cabo13@gmail.com>
2022-07-01 18:09:40 +0200

    1. sum of positive

--- script.js
+++ script.js
@@ -5,23 +5,12 @@
 // Example [1,-4,7,12] => 1 + 7 + 12 = 20
 // Note: if there is nothing to sum, the sum is default to 0.
 
-// function positiveSum(arr) {
-//   let sum = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] >= 0) {
-//       sum += arr[i];
-//     }
-//   }
-//   return sum;
-// }
-
-// 2. STRING REPEAT
-// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
-
-function repeatStr(n, s) {
-  let string = "";
-  for (let i = 0; i < n; i++) {
-    string += s;
+function positiveSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0) {
+      sum += arr[i];
+    }
   }
-  return string;
+  return sum;
 }
