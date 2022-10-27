9b2041ed485e54598995e31a0ae4192df4c6e9ae
Marko <marko.cabo13@gmail.com>
2022-10-28 01:11:56 +0200

    16. Multiples of 3 or 5 - Completed

--- script.js
+++ script.js
@@ -324,3 +324,37 @@
 // let str1 = "rqodlw";
 // let str2 = "world";
 // console.log(scramble(str1, str2));
+
+// 16. MULTIPLES OF 3 OR 5
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number) {
+  let arr = [];
+
+  if (number < 0) return 0;
+
+  // Check every number and decreasing by one, push into array
+  for (let i = 1; i < number; i++) {
+    let lowerNumber = number - i;
+    if (lowerNumber % 3 === 0 || lowerNumber % 5 === 0) {
+      arr.push(lowerNumber);
+    }
+  }
+
+  // If there are elements in array
+  if (arr.length > 0)
+    // 1. sort array
+    // 2. filter new array where the element is not the same as previous
+    // 3. reduce the array: sum all values
+    return arr
+      .sort()
+      .filter(function (el, index, arr) {
+        return el !== arr[index - 1];
+      })
+      .reduce((previousValue, currentValue) => previousValue + currentValue);
+
+  //If no elements in array return 0
+  return 0;
+}
