256c708bcf3ea9933456636582c5c0bf8d10944c
Marko1310 <marko.cabo13@gmail.com>
2023-05-13 00:17:52 +0200

    2 sums

--- script.js
+++ script.js
@@ -672,10 +672,38 @@
 // Output: [42,42,42]
 // Explanation: The function always returns 42.
 
-var map = function (arr, fn) {
-  let temp = [];
-  for (i = 0; i < arr.length; i++) {
-    temp.push(fn(arr[i], i));
+// var map = function (arr, fn) {
+//   let temp = [];
+//   for (i = 0; i < arr.length; i++) {
+//     temp.push(fn(arr[i], i));
+//   }
+//   return temp;
+// };
+
+// 26. Two Sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+var twoSum = function (nums, target) {
+  let indexes = [];
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] + nums[i + 1] === target) {
+      indexes.push(i, i + 1);
+    }
   }
-  return temp;
+  return indexes;
 };
