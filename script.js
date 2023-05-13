9b08938a414f2d41dbb1879395bfc7c5f84fe5a3
Marko1310 <marko.cabo13@gmail.com>
2023-05-13 17:53:52 +0200

    Haystack

--- script.js
+++ script.js
@@ -784,3 +784,25 @@
 // };
 
 // console.log(isValid('[[]()]'));
+
+//29. Find the Index of the First Occurrence in a String
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Example 1:
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+
+var strStr = function (haystack, needle) {
+  const array = haystack.split(needle).join(' ');
+  console.log(array);
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === ' ') {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log(strStr('hello', 'll'));
