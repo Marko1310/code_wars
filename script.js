3dd7a90e36157f2aeb09730ac28c669dfb5c869e
Marko1310 <marko.cabo13@gmail.com>
2023-06-03 17:25:29 +0200

    39. Merge sorted

--- script.js
+++ script.js
@@ -1013,9 +1013,43 @@
 // Incrementing by one gives 123 + 1 = 124.
 // Thus, the result should be [1,2,4].
 
-var plusOne = function (digits) {
-  const newArr = (BigInt(digits.join('')) + BigInt(1)).toString().split('');
-  return newArr;
+// var plusOne = function (digits) {
+//   const newArr = (BigInt(digits.join('')) + BigInt(1)).toString().split('');
+//   return newArr;
+// };
+
+// console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+
+//39. Merge Sorted Array
+//You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+// Example 1:
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+var merge = function (nums1, m, nums2, n) {
+  let first = m - 1;
+  let second = n - 1;
+  let i = m + n - 1;
+  while (second >= 0) {
+    let firstVal = nums1[first];
+    let secondVal = nums2[second];
+
+    if (firstVal > secondVal) {
+      nums1[i] = firstVal;
+      i--;
+      first--;
+    } else {
+      nums1[i] = secondVal;
+      i--;
+      second--;
+    }
+  }
+  console.log(nums1);
 };
 
-console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
