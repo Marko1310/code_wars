bae9ad49bc46bca65feb0f8f632e633070905c21
Marko1310 <marko.cabo13@gmail.com>
2023-06-03 16:22:43 +0200

    38. Plus One

--- script.js
+++ script.js
@@ -1001,3 +1001,21 @@
 
 // // console.log(isPalindrome('A man, a plan, a canal: Panama'));
 // console.log(isPalindrome('A man, a plan, a canal: Panama'));
+
+// 38. Plus one
+//You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+// Increment the large integer by one and return the resulting array of digits.
+
+// Example 1:
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+var plusOne = function (digits) {
+  const newArr = (BigInt(digits.join('')) + BigInt(1)).toString().split('');
+  return newArr;
+};
+
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
