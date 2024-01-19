23ae22744d3ad4ee322cc5660423c72d4ed09aa2
Marko1310 <marko.cabo13@gmail.com>
2024-01-19 18:06:14 +0100

    41. Finde middle node

--- script.js
+++ script.js
@@ -1097,13 +1097,44 @@
 // Input: head = []
 // Output: []
 
-const reverseList = function (head) {
-	let prev = null;
+// const reverseList = function (head) {
+// 	let prev = null;
+// 	let current = head;
+// 	while (current !== null) {
+// 		const next = current.next;
+// 		current.next = prev;
+// 		prev = current;
+// 		current = next;
+// 	}
+// };
+
+//41. Middle of the linked list
+//Given the head of a singly linked list, return the middle node of the linked list.
+// If there are two middle nodes, return the second middle node.
+
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [3,4,5]
+// Explanation: The middle node of the list is node 3.
+
+// Example 2:
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+const middleNode = function (head) {
+	let counter = 0;
 	let current = head;
 	while (current !== null) {
-		const next = current.next;
-		current.next = prev;
-		prev = current;
-		current = next;
+		counter++;
+		current = current.next;
 	}
+
+	current = head;
+	const max = Math.floor(counter / 2);
+
+	for (let i = 0; i < max; i++) {
+		current = current.next;
+	}
+	return current;
 };
