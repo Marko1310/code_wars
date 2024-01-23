fa2b1c26e8b33697f6df7c5fc8d214169f37f43f
Marko1310 <marko.cabo13@gmail.com>
2024-01-23 09:40:47 +0100

    49. Remove duplicates

--- script.js
+++ script.js
@@ -1341,16 +1341,39 @@
 // Output: ""
 // Explanation: There is no common prefix among the input strings.
 
-const longestCommonPrefix = (arrayStrings) => {
-	arrayStrings.sort();
-	let prefix = '';
-	if (arrayStrings.length === 0) return prefix;
-	for (let i = 0; i < arrayStrings[0].length; i++) {
-		const character = arrayStrings[0][i];
-		for (let j = 1; j < arrayStrings.length; j++) {
-			if (arrayStrings[j][i] !== character) return prefix;
+// const longestCommonPrefix = (arrayStrings) => {
+// 	arrayStrings.sort();
+// 	let prefix = '';
+// 	if (arrayStrings.length === 0) return prefix;
+// 	for (let i = 0; i < arrayStrings[0].length; i++) {
+// 		const character = arrayStrings[0][i];
+// 		for (let j = 1; j < arrayStrings.length; j++) {
+// 			if (arrayStrings[j][i] !== character) return prefix;
+// 		}
+// 		prefix = prefix + character;
+// 	}
+// 	return prefix;
+// };
+
+// 49. Remove duplicates from sorted Array
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+// Example 1:
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+const removeDuplicates = function (nums) {
+	let k = 1;
+	for (let i = 0; i < nums.length - 1; i++) {
+		if (nums[i] !== nums[i + 1]) {
+			nums[k] = nums[i + 1];
+			k++;
 		}
-		prefix = prefix + character;
 	}
-	return prefix;
+	return k;
 };
