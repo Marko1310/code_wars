45fad3307b2514efb4f9383b284feb7b1a1681a3
Marko1310 <marko.cabo13@gmail.com>
2024-01-23 11:14:53 +0100

    Remove element

--- script.js
+++ script.js
@@ -1367,13 +1367,37 @@
 // Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 
-const removeDuplicates = function (nums) {
-	let k = 1;
-	for (let i = 0; i < nums.length - 1; i++) {
-		if (nums[i] !== nums[i + 1]) {
-			nums[k] = nums[i + 1];
+// const removeDuplicates = function (nums) {
+// 	let k = 1;
+// 	for (let i = 0; i < nums.length - 1; i++) {
+// 		if (nums[i] !== nums[i + 1]) {
+// 			nums[k] = nums[i + 1];
+// 			k++;
+// 		}
+// 	}
+// 	return k;
+// };
+
+//50. Remove element
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+// Example 1:
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+const removeElement = function (nums, val) {
+	let k = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] !== val) {
+			nums[k] = nums[i];
 			k++;
 		}
 	}
-	return k;
 };
+
+removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2);
