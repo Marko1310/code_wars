3c0ba9c78ad5c1e219f03ab95ac40fea3ee5f6b6
Marko1310 <marko.cabo13@gmail.com>
2024-02-01 00:15:17 +0100

    54. Climbing stairs - recursion

--- script.js
+++ script.js
@@ -1489,14 +1489,6 @@ const removeElement = function (nums, val) {
 // 3. 2 steps + 1 step
 
 const climbStairs = function (n) {
-	if (n === 0) return 0;
-	let n1 = 0,
-		n2 = 1,
-		nextTerm;
-	for (let i = 0; i < n; i++) {
-		nextTerm = n1 + n2;
-		n1 = n2;
-		n2 = nextTerm;
-	}
-	return nextTerm;
+	if (n < 3) return n;
+	return climbStairs(n - 1) + climbStairs(n - 2);
 };
