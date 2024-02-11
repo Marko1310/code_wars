52246df884f9f218739abee33dd15119ec7d8489
Marko1310 <marko.cabo13@gmail.com>
2024-02-11 19:50:33 +0100

    55. Single number

--- script.js
+++ script.js
@@ -1488,7 +1488,22 @@ const removeElement = function (nums, val) {
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
 
-const climbStairs = function (n) {
-	if (n < 3) return n;
-	return climbStairs(n - 1) + climbStairs(n - 2);
-};
+// const climbStairs = function (n) {
+// 	if (n < 3) return n;
+// 	return climbStairs(n - 1) + climbStairs(n - 2);
+// };
+
+//55.Single Number
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Example 1:
+// Input: nums = [2,2,1]
+// Output: 1
+
+// Example 2:
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+
+const singleNumber = function (nums) {};
+console.log(singleNumber([2, 2, 2, 1]));
