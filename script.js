622a1554b5e0b41fb45ea47629f03de755971791
Marko1310 <marko.cabo13@gmail.com>
2024-02-12 23:56:01 +0100

    56. Majority Element

--- script.js
+++ script.js
@@ -1505,15 +1505,29 @@ const removeElement = function (nums, val) {
 // Input: nums = [4,1,2,1,2]
 // Output: 4
 
-const singleNumber = function (nums) {
-	let hashMap = {};
-	for (let i = 0; i < nums.length; i++) {
-		if (hashMap[nums[i]]) hashMap[nums[i]] = false;
-		else hashMap[nums[i]] = true;
-	}
+// const singleNumber = function (nums) {
+// 	let hashMap = {};
+// 	for (let i = 0; i < nums.length; i++) {
+// 		if (hashMap[nums[i]]) hashMap[nums[i]] = false;
+// 		else hashMap[nums[i]] = true;
+// 	}
 
-	for (let [key, value] of Object.entries(hashMap)) {
-		if (value) return key;
-	}
-};
-console.log(singleNumber([2, 2, 3, 3, 4, 1]));
+// 	for (let [key, value] of Object.entries(hashMap)) {
+// 		if (value) return key;
+// 	}
+// };
+// console.log(singleNumber([2, 2, 3, 3, 4, 1]));
+
+// 56. Majority Element
+// Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+// Input: nums = [3,2,3]
+// Output: 3
+
+// Example 2:
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+const majorityElement = function (nums) {};
