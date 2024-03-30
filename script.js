b367b83da0407892ee406a6b4e6738b4f8ff87ea
Marko1310 <marko.cabo13@gmail.com>
2024-03-31 00:48:37 +0100

    61. Shuffle the array

--- script.js
+++ script.js
@@ -1710,16 +1710,39 @@
 // Input: nums = [0,1,2,2,3,0,4,2], val = 2
 // Output: 5, nums = [0,1,4,0,3,_,_,_]
 
-const removeElement = function (nums, val) {
-	let k = 0;
-	for (let i = 0; i < nums.length; i++) {
-		if (nums[i] !== val) {
-			nums[k] = nums[i];
-			k++;
-		}
+// const removeElement = function (nums, val) {
+// 	let k = 0;
+// 	for (let i = 0; i < nums.length; i++) {
+// 		if (nums[i] !== val) {
+// 			nums[k] = nums[i];
+// 			k++;
+// 		}
+// 	}
+// 	console.log(nums);
+// };
+
+// const arr = [3, 2, 2, 3];
+// removeElement(arr, 2);
+
+//61. Shuffle the array
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+// Example 1:
+// Input: nums = [2,5,1,3,4,7], n = 3
+// Output: [2,3,5,4,1,7]
+// Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+const shuffle = function (nums, n) {
+	const arr = [];
+	let k = n;
+	for (let i = 0; i < n; i++) {
+		arr.push(nums[i]);
+		arr.push(nums[k]);
+		k++;
 	}
-	console.log(nums);
+	return arr;
 };
 
-const arr = [3, 2, 2, 3];
-removeElement(arr, 2);
+const arr = [2, 5, 1, 3, 4, 7];
+shuffle(arr, 3);
