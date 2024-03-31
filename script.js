4ad56c61816134ac27a545839cce984f0e743dfa
Marko1310 <marko.cabo13@gmail.com>
2024-03-31 15:01:06 +0200

    66. Two Sum

--- script.js
+++ script.js
@@ -1854,35 +1854,66 @@
 // Input: s = "rat", t = "car"
 // Output: false
 
-const validAnagram = function (s, t) {
-	const hashS = {};
-	const hashT = {};
-	for (let letter of s) {
-		if (!hashS[letter]) {
-			hashS[letter] = 1;
-		} else {
-			hashS[letter]++;
-		}
-	}
+// const validAnagram = function (s, t) {
+// 	const hashS = {};
+// 	const hashT = {};
+// 	for (let letter of s) {
+// 		if (!hashS[letter]) {
+// 			hashS[letter] = 1;
+// 		} else {
+// 			hashS[letter]++;
+// 		}
+// 	}
 
-	for (let letter of t) {
-		if (!hashS[letter]) {
-			return false;
-		} else {
-			if (!hashT[letter]) {
-				hashT[letter] = 1;
-			} else {
-				hashT[letter]++;
-			}
-		}
-	}
+// 	for (let letter of t) {
+// 		if (!hashS[letter]) {
+// 			return false;
+// 		} else {
+// 			if (!hashT[letter]) {
+// 				hashT[letter] = 1;
+// 			} else {
+// 				hashT[letter]++;
+// 			}
+// 		}
+// 	}
 
-	for (let ch in hashS) {
-		if (hashS[ch] !== hashT[ch]) {
-			return false;
+// 	for (let ch in hashS) {
+// 		if (hashS[ch] !== hashT[ch]) {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// };
+
+// console.log(validAnagram('anagram', 'nagaram'));
+
+//66. Two Sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+const twoSum = function (nums, target) {
+	const hash = {};
+	for (let i = 0; i < nums.length; i++) {
+		const currentNumber = nums[i];
+		const neededValue = target - currentNumber;
+		const index = hash[neededValue];
+
+		if (index !== undefined) {
+			return [index, i];
+		} else {
+			hash[currentNumber] = i;
 		}
 	}
-	return true;
 };
 
-console.log(validAnagram('anagram', 'nagaram'));
+console.log(twoSum([2, 7, 11, 15], 9));
