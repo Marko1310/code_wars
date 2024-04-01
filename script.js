012c21d0ae144d6ad47e03614ba4453337e34f48
Marko1310 <marko.cabo13@gmail.com>
2024-04-01 18:07:00 +0200

    69. Happy Number

--- script.js
+++ script.js
@@ -1957,16 +1957,63 @@
 // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 // Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
-const maxProfit = function (prices) {
-	let buyPrice = prices[0];
-	let profit = 0;
+// const maxProfit = function (prices) {
+// 	let buyPrice = prices[0];
+// 	let profit = 0;
 
-	for (let i = 0; i < prices.length; i++) {
-		if (prices[i] < buyPrice) buyPrice = prices[i];
-		if (prices[i] - buyPrice > profit) profit = prices[i] - buyPrice;
+// 	for (let i = 0; i < prices.length; i++) {
+// 		if (prices[i] < buyPrice) buyPrice = prices[i];
+// 		if (prices[i] - buyPrice > profit) profit = prices[i] - buyPrice;
+// 	}
+// 	console.log(profit);
+// };
+
+// const prices = [2, 4, 1];
+// maxProfit(prices);
+
+//69. Happy Number
+// Write an algorithm to determine if a number n is happy.
+// A happy number is a number defined by the following process:
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+// Example 1:
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+
+// Example 2:
+// Input: n = 2
+// Output: false
+
+const happyNumber = function (n) {
+	const visited = new Set();
+	let sum = n;
+
+	while (sum !== 1) {
+		if (visited.has(sum)) return false;
+		if (!visited.has(sum)) {
+			visited.add(sum);
+			sum = sumOfSquares(sum);
+		}
 	}
-	console.log(profit);
+	return true;
 };
 
-const prices = [2, 4, 1];
-maxProfit(prices);
+function sumOfSquares(n) {
+	let sum = 0;
+	while (n > 0) {
+		let digit = n % 10;
+		sum += digit ** 2;
+		n = Math.floor(n / 10);
+	}
+	return sum;
+}
+
+console.log(happyNumber(2));
