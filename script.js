f964ad1afb7c617b23ccde31d80e1ab180588767
Marko1310 <marko.cabo13@gmail.com>
2024-04-05 12:30:01 +0200

    75. Nim Game

--- script.js
+++ script.js
@@ -2135,28 +2135,57 @@
 // Input: pattern = "abba", s = "dog cat cat fish"
 // Output: false
 
-const wordPattern = function (pattern, s) {
-	const stringArray = s.split(' ');
-	if (stringArray.length !== pattern.length) {
-		return false;
-	}
+// const wordPattern = function (pattern, s) {
+// 	const stringArray = s.split(' ');
+// 	if (stringArray.length !== pattern.length) {
+// 		return false;
+// 	}
 
-	const hash = {};
-	for (let i = 0; i < stringArray.length; i++) {
-		if (
-			(hash[pattern[i]] && hash[pattern[i]] !== stringArray[i]) ||
-			(!hash[pattern[i]] && Object.values(hash).includes(stringArray[i]))
-		)
-			return false;
+// 	const hash = {};
+// 	for (let i = 0; i < stringArray.length; i++) {
+// 		if (
+// 			(hash[pattern[i]] && hash[pattern[i]] !== stringArray[i]) ||
+// 			(!hash[pattern[i]] && Object.values(hash).includes(stringArray[i]))
+// 		)
+// 			return false;
 
-		hash[pattern[i]] = stringArray[i];
+// 		hash[pattern[i]] = stringArray[i];
 
-		return true;
-	}
+// 		return true;
+// 	}
 
-	return true;
+// 	return true;
+// };
+
+// const pattern = 'abba';
+// const s = 'dog constructor constructor dog';
+// wordPattern(pattern, s);
+
+//75. Nim Game
+// You are playing the following Nim Game with your friend:
+// Initially, there is a heap of stones on the table.
+// You and your friend will alternate taking turns, and you go first.
+// On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
+// The one who removes the last stone is the winner.
+// Given n, the number of stones in the heap, return true if you can win the game assuming both you and your friend play optimally, otherwise return false.
+
+// Example 1:
+// Input: n = 4
+// Output: false
+// Explanation: These are the possible outcomes:
+// 1. You remove 1 stone. Your friend removes 3 stones, including the last stone. Your friend wins.
+// 2. You remove 2 stones. Your friend removes 2 stones, including the last stone. Your friend wins.
+// 3. You remove 3 stones. Your friend removes the last stone. Your friend wins.
+// In all outcomes, your friend wins.
+
+// Example 2:
+// Input: n = 1
+// Output: true
+
+// Example 3:
+// Input: n = 2
+// Output: true
+
+const canWinNim = function (n) {
+	return n % 4 !== 0;
 };
-
-const pattern = 'abba';
-const s = 'dog constructor constructor dog';
-wordPattern(pattern, s);
