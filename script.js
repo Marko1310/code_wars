684a8719486202a8a7f57a6c738457fc586fa424
Marko1310 <marko.cabo13@gmail.com>
2024-04-06 16:07:47 +0200

    85. Longest Palindrome

--- script.js
+++ script.js
@@ -2457,19 +2457,44 @@
 // Input: s = "axc", t = "ahbgdc"
 // Output: false
 
-const isSubsequence = (s, t) => {
-	let pointerS = 0;
-	let pointerT = 0;
+// const isSubsequence = (s, t) => {
+// 	let pointerS = 0;
+// 	let pointerT = 0;
 
-	while (pointerS < s.length && pointerT < t.length) {
-		if (s[pointerS] === t[pointerT]) {
-			pointerS++;
+// 	while (pointerS < s.length && pointerT < t.length) {
+// 		if (s[pointerS] === t[pointerT]) {
+// 			pointerS++;
+// 		}
+// 		pointerT++;
+// 	}
+// 	return pointerS === s.length;
+// };
+
+// const s = 'b';
+// const t = 'c';
+// isSubsequence(s, t);
+
+//85. Longest Palindrome
+// Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+// Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+// Example 1:
+// Input: s = "abccccdd"
+// Output: 7
+// Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+const longestPalindrome = (s) => {
+	let answer = 0;
+	const hash = {};
+	for (let char of s) {
+		hash[char] = (hash[char] || 0) + 1;
+		if (hash[char] % 2 === 0) {
+			answer += 2;
 		}
-		pointerT++;
 	}
-	return pointerS === s.length;
+
+	return answer === s.length ? answer : answer + 1;
 };
 
-const s = 'b';
-const t = 'c';
-isSubsequence(s, t);
+const s = 'abccccdd';
+longestPalindrome(s);
